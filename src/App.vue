2370a5054ea3853255f5cbc93cf14f949b695c7f
esther-v <esvalas@gmail.com>
2021-11-27 15:00:56 +0100

    display data and change bg

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main !='undefined' && weather.main.temp > 16 ? 'warm' : '' ">
     <main>
       <div class="search-box">
         <input 
@@ -14,11 +14,11 @@
       <div class="weather-app" v-if="typeof weather.main !='undefined' ">
         <div class="location-box">
           <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
-          <div class="date">Monday 29 November 2021</div>
+          <div class="date">{{ dateBuilder() }}</div>
         </div>
         <div class="weather-box">
-          <div class="temp">9°c</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -47,6 +47,18 @@ export default {
     },
     setResults (results) {
       this.weather = results
+    },
+    dateBuilder() {
+      let d = new Date()
+      let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+      let day = days[d.getDay()]
+      let date = d.getDate()
+      let month = months[d.getMonth()]
+      let year = d.getFullYear()
+
+      return `${day} ${date} ${month} ${year}`
     }
   }
 }
@@ -68,6 +80,10 @@ body{
   background-size: cover;
   background-position: bottom;
   transition: 0.4s;
+}
+
+#app.warm{
+  background-image: url('./assets/warm-bg.jpeg')
 }
 
 main{
@@ -145,3 +161,4 @@ main{
 }
 
 </style>
+
