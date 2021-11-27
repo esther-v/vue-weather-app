f448dea7f20aea47bad92acf6f6dba9e2788b74f
esther-v <esvalas@gmail.com>
2021-11-27 11:45:32 +0100

    starting project ⚡️

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data(){
+    return{
+      api_key
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
